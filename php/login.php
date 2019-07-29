178f467bdaa4c52a78930fc6fc5fcbbf87ebf0b4
giacomo <giacfuria@gmail.com>
2019-07-29 21:31:28 +0200

    Fine implementazione funzione authenticate

--- php/login.php
+++ php/login.php
@@ -7,12 +7,27 @@
 
 	echo 'Username: '.$username.', password: '.$password;
 
+	/* Funzione che verifica se l'utente è presente nel database.
+		Se l'utente è presente allora restituisce il suo userId
+		altrimenti restituisce -1.
+	*/
 	function authenticate($username, $password){
 		global $bookMyAppointmentDb;
 
 		$username = $bookMyAppointmentDb->sqlInjectionFilter($username);
 		$password = $bookMyAppointmentDb->sqlInjectionFilter($password);
 
-		$queryText = "SELECT * FROM user WHERE username=";
+		$queryText = "SELECT * FROM user WHERE username='".$username."' AND password='".$password."'";
+
+		$result = $bookMyAppointmentDb->performQuery($queryText);
+		$numRow = mysqli_num_rows($result);
+		if($numRow != 1)
+			return -1;
+
+		$bookMyAppointmentDb->closeConnection();
+		$userRow = $result->fetch_assoc();
+		$bookMyAppointmentDb->closeConnection();
+		return $userRow['userId'];
+
 	}
 ?>
