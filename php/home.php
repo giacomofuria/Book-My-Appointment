2240d11dd558dd730053beb9b18f119f38198403
giacomo <giacfuria@gmail.com>
2019-07-30 19:05:27 +0200

    Aggiunto il file logout.php che distrugge la sessione e aggiunta del link a logout.php nella pagina home.php

--- php/home.php
+++ php/home.php
@@ -1,4 +1,14 @@
 <?php
 	session_start();
-	echo '<u>Benvenuto:</u> '.$_SESSION['email'].', <u>userId:</u> '.$_SESSION['userId'];
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Home Page - Book My Appointment</title>
+</head>
+<body>
+	<?php echo '<u>Benvenuto:</u> '.$_SESSION['email'].', <u>userId:</u> '.$_SESSION['userId']; ?>
+	<span><p><a href="./logout.php">Sign out</a></p><span>
+</body>
+</html>
