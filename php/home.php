0c569fe0a5d8e641ff7c5a39b242eaaf3aa6ec2e
giacomo <giacfuria@gmail.com>
2019-08-02 21:38:32 +0200

    Sto cercando di stabilizzare lo stile della pagina home e di tutte le pagine in cui l'utente deve fare cose

--- php/home.php
+++ php/home.php
@@ -16,16 +16,17 @@
 	<link rel="stylesheet" href="./../css/menu.css" type="text/css" media="screen">
 </head>
 <body>
-	<div class="container">
-		<div id="left-side">
-			<div class="menu">
-				
-			</div>
+	<div id="left-side">
+		<div class="menu">
+			Menu
 		</div>
-		<div id="right-side">
+	</div>
+	<div id="right-side">
+		<div id="top-bar-container">Top-Bar</div>
+		<div id="workspace-container">
+			Workspace<br><br>
 			<?php echo '<u>Benvenuto:</u> '.$_SESSION['email'].', <u>userId:</u> '.$_SESSION['userId'];  ?>
 			<span><p><a href="./logout.php">Sign out</a></p></span>
-			
 		</div>
 	</div>
 </body>
