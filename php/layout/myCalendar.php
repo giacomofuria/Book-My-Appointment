edb2e34d652dceedefbf39f9de570c1c7b1c87e3
giacomo <giacfuria@gmail.com>
2019-08-07 08:10:12 +0200

    Sviluppo calendario php

--- php/layout/myCalendar.php
+++ php/layout/myCalendar.php
@@ -13,16 +13,31 @@
 			border-width: 1px;
 			border-color: black;
 			text-align: center;
-			padding: 1em;
+			padding: 0.5em;
+		}
+		button{
+			background-color: #FFFFFF;
+			width: 100%;
+			height: 100%;
+			font-size: 18px;
 		}
 	</style>
 </head>
 <body>
 
 <?php
-	$year_month = date('Y-m');// Anno e mese attuali
-
+	if(isset($_GET['ym'])){
+		$year_month = $_GET['ym'];
+	}else{
+		$year_month = date('Y-m');// Anno e mese attuali
+	}
 	$timestamp = strtotime($year_month.'-01'); // timestamp relativo al primo giorno del mese attuale
+
+	// controllo la validità del timestamp
+	if($timestamp === false){
+		$year_month = date('Y-m');
+		$timestamp = strtotime($year_month.'-01');
+	}
 
 	$today = date('Y-m-j',time()); // Data corrente
 
@@ -30,10 +45,21 @@
 
 	$numeroPrimoGiornoDelMese = date('w',$timestamp);
 
+	$timestampPrimoGiornoMesePrecedente = mktime(0,0,0,(date('m',$timestamp)-1),1,(date('Y',$timestamp)));
+	$timestampPrimoGiornoMeseSuccessivo = mktime(0,0,0,(date('m',$timestamp)+1),1,(date('Y',$timestamp)));
+
+	$prev = date('Y-m', $timestampPrimoGiornoMesePrecedente);
+	$next = date('Y-m', $timestampPrimoGiornoMeseSuccessivo);
+
 	echo 'Year-Month: '.$year_month.',<br>today: '.$today.',<br>in questo mese ci sono '.$daysInMonth.' giorni,<br>il primo giorno del mese e\': '.$numeroPrimoGiornoDelMese.'<br>';
 
 	$weeks = array();
 	$week = '';
+
+	// Inserisco i link al giorno precedente e a quello successivo
+
+	echo "<p><a href='?ym=$prev' >mese precedente</a></p>";
+	echo '<p><a href=\'?ym='.$next.'\' >mese successivo</a></p>';
 
 	echo '<table>';
 
@@ -47,12 +73,15 @@
 	<th>Sun</th>
 	</tr>';
 
+	if($numeroPrimoGiornoDelMese == 0)
+		$numeroPrimoGiornoDelMese = 7;
+
 	$week = $week.str_repeat('<td></td>',$numeroPrimoGiornoDelMese - 1);
 	$index = $numeroPrimoGiornoDelMese;
 	for($day = 1; $day <= $daysInMonth; $day++, $index++){
 		$date = $year_month.'-'.$day; // creo la data
 
-		$week.= '<td>'.$day.'</td>';
+		$week.= '<td><button>'.$day.'</button></td>';
 
 		// Fine della settimana o fine del mese
 		if(($index % 7) == 0 || $day == $daysInMonth){
