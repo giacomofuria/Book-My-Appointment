eb58349b295c5232e565dacfd5b5f7c9d6ea11db
giacomo <giacfuria@gmail.com>
2019-08-31 23:05:01 +0200

    Definizione della tabella StrutturaTabellaAppuntamenti per memorizzare i dati della struttura della tabella degli appuntamenti di ogni utente

--- book_my_appointment.sql
+++ book_my_appointment.sql
@@ -11,4 +11,24 @@ CREATE TABLE `user` (
   `password` varchar(64) NOT NULL,
   PRIMARY KEY (`userId`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+
+CREATE TABLE StrutturaTabellaAppuntamenti (
+    userId int(11) NOT NULL,
+    lun BOOLEAN DEFAULT FALSE,
+    mar BOOLEAN DEFAULT FALSE,
+    mer BOOLEAN DEFAULT FALSE,
+    gio BOOLEAN DEFAULT FALSE,
+    ven BOOLEAN DEFAULT FALSE,
+    sab BOOLEAN DEFAULT FALSE,
+    dom BOOLEAN DEFAULT FALSE,
+    oraInizio int(11) DEFAULT NULL,
+    oraFine int(11) DEFAULT NULL,
+    durataIntervalli int(11) DEFAULT 60,
+    intervalliPausa VARCHAR(100) DEFAULT NULL,
+    PRIMARY KEY (userId),
+    CONSTRAINT userId FOREIGN KEY (userID)
+    REFERENCES user(userId)
+    ON UPDATE NO ACTION
+    ON DELETE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
